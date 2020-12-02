be4f9960b53ac13d2641d876bbd150913c975d71
jose.castro <jose.castro@meupag.com.br>
2020-12-01 21:01:51 -0300

    fix

--- app/src/main/java/com/example/mytvmaze/api/repository/ApiRepositoryImpl.kt
+++ app/src/main/java/com/example/mytvmaze/api/repository/ApiRepositoryImpl.kt
@@ -3,7 +3,6 @@ package com.example.mytvmaze.api.repository
 import android.content.Context
 import com.example.mytvmaze.api.service.ApiService
 import com.example.mytvmaze.core.network.retrofit.model.RetrofitResponse
-import com.example.mytvmaze.core.network.retrofit.toResource
 
 class ApiRepositoryImpl(val context: Context, private val api: ApiService) : ApiRepository {
 
